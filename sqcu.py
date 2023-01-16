07791455714b16722147692fe0b4839316637312
balakrishnaraja123 <122012417+balakrishnaraja123@users.noreply.github.com>
2023-01-16 15:59:19 +0530

    Add files via upload

---
+++ sqcu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+list1=[2,3,4,6,8,10]
+square=[i**3 for i in list1]
+print('Square of the numbers are:',square)
+
+
+# In[17]:
+
+
+cube=[i**3 for i in list1]
+print('Cube of the numbers are:',cube)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
